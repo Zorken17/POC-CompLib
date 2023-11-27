cf47e79cfbaefeef693222aea08a2e4b8b362b93
Zoltan Darvas <>
2023-11-27 17:01:49 +0100

    rollup test

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,7 +1,16 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: ['src/index.ts'],
+  // input: [
+  //   'src/index.ts',
+  //   'src/1-atoms/Button/index.ts',
+  //   'src/1-atoms/Label/index.ts',
+  //   'src/2-molecules/Test/index.ts',
+  // ],
+  input: {
+    index: 'src/index.ts',
+    button: 'src/1-atoms/Button/index.ts',
+  },
   output: {
     dir: 'lib',
     format: 'cjs',
