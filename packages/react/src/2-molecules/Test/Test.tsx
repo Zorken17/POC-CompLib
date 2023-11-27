29f22ced656fdcd0f6089b9767baceac04508893
Zoltan Darvas <>
2023-11-27 15:13:18 +0100

    .

--- packages/react/src/2-molecules/Test/Test.tsx
+++ packages/react/src/2-molecules/Test/Test.tsx
@@ -3,7 +3,7 @@ import { Button, Lable } from '../../1-atoms';
 export const Test = () => {
   return (
     <>
-      <h1>This is a TEST molecule</h1>
+      <h1>This is a TEST molecule!</h1>
       <Lable id='2' fontWeight='bold'>
         This is a Label
       </Lable>
