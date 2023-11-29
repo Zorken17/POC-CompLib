d58a001ff2969ef3d9ea6c37c803a0e6c84c9065
Zoltan Darvas <>
2023-11-29 10:41:25 +0100

    added molecules

---
+++ packages/react/src/2-molecules/rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: ['./index.ts'],
+  output: {
+    dir: 'lib',
+    format: 'cjs',
+    sourcemap: true,
+    preserveModules: true,
+  },
+  plugins: [typescript()],
+  external: ['react/jsx-runtime', 'react'],
+};
